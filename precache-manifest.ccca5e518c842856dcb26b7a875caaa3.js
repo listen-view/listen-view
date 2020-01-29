2e0d6167a0a21caa50db231efcdb0a22b5ed6552
xiaoxiangnan <asd983302356@163.com>
2020-01-29 17:59:12 +0800

    init

---
+++ precache-manifest.ccca5e518c842856dcb26b7a875caaa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82539d4f325ba8e916dccf47a5323c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2666087cae670ba25052",
+    "url": "./static/css/main.d3b5b9c1.chunk.css"
+  },
+  {
+    "revision": "985bac840a39fd62e3dd",
+    "url": "./static/js/2.ccd18ea2.chunk.js"
+  },
+  {
+    "revision": "2666087cae670ba25052",
+    "url": "./static/js/main.826fc981.chunk.js"
+  },
+  {
+    "revision": "7bf3b8bc4458990e0363",
+    "url": "./static/js/runtime-main.2670e288.js"
+  }
+]);
